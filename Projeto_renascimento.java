4459c6e69a45d8f3e9c55d499d40130bb7b45bc6
Andresharck <andrevitor219@gmail.com>
2026-03-24 20:26:59 -0300

    Add files via upload

---
+++ Projeto_renascimento.java
@@ -0,0 +1,13 @@
+public class Projeto_renascimento {
+    public static void main(String[] args) {
+        boolean possuiCoragem = true;
+        int saudeFisica = 40;
+            if (possuiCoragem && saudeFisica > 40) {
+            System.out.println("Você esta pronto para entrar os desafios do renasciemnto ");
+            } else {
+                System.out.println("Você precisa de mais coragem é saude fisica para enfrentar os desafios do renascimento ");
+            }
+        }
+
+    }
+
